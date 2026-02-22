eabec904905f210bb722810148c00fd7730525e9
jacisko1 <jakub.jacisko@gmail.com>
2026-02-22 16:40:28 +0100

    Remove intro text section from homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,10 @@
 import { SectionCard } from "../components/SectionCard";
-import { useLanguage } from "../data/language";
 import { navigationTree } from "../data/navigation";
 import styles from "./HomePage.module.css";
 
 export default function HomePage() {
-  const { t } = useLanguage();
-
   return (
     <section className={styles.wrap}>
-      <header className={styles.header}>
-        <p>{t("homeIntro")}</p>
-      </header>
-
       <div className={styles.grid}>
         {navigationTree.map((section) => (
           <SectionCard key={section.path} section={section} />
