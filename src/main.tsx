72f8ee560a3c420d6a8dc3140a1dcd317356494a
jacisko1 <jakub.jacisko@gmail.com>
2026-02-23 21:24:12 +0100

    fix(pwa): auto-refresh mobile app after Vercel deploy

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,19 @@ import App from "./App";
 import "./styles/global.css";
 
 if (import.meta.env.PROD) {
-  registerSW({ immediate: true });
+  const updateSW = registerSW({
+    immediate: true,
+    onNeedRefresh() {
+      updateSW(true);
+    },
+    onRegisteredSW(_swUrl, registration) {
+      if (registration) {
+        setInterval(() => {
+          registration.update();
+        }, 60 * 1000);
+      }
+    }
+  });
 } else if ("serviceWorker" in navigator) {
   navigator.serviceWorker.getRegistrations().then((registrations) => {
     registrations.forEach((registration) => {
